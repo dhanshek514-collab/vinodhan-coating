d90e49685b8a3e7ff1c7c7cd40a49d53fe7484b7
dhanshek514-collab <dhanshek514@gmail.com>
2026-04-15 10:49:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {}
-  }
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000,
+  },
 })
